254567944d2ce0456fe93ab59452a5b0b7f5f15b
Liang <liang@imac.local>
2017-07-07 11:36:50 +0200

    Adds a sample project for using this plugin

---
+++ examples/Hello/Hello/Hello.m
@@ -0,0 +1,21 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 06-Jun-2017 *)
+
+(*BeginPackage["Hello`"]*)
+(* Exported symbols added here with SymbolName::usage *)(* *)
+
+(*Begin["`Private`"]*)
+(* Implementation of the package *)
+
+
+(*End[]*)
+
+(*EndPackage[]*)
+
+cuboid[] := Graphics3D[Cuboid[]];
+sphere[] := Graphics3D[Sphere[]];
+wg[]:= WheelGraph[8];
+{wg[],sphere[], cuboid[]}
+(*cuboid[]*)
+
